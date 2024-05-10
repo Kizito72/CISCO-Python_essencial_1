5f8d917e10e4dc6a80f677c7f821d0c3e3466e31
Kizito Ngonye <kizitorawbiz@gmail.com>
2024-05-10 23:19:46 +0100

    Formatting the output

--- LAB_2.1.13___Formatting the output/arrow_dublicare.py
+++ LAB_2.1.13___Formatting the output/arrow_dublicare.py
@@ -1,12 +1,20 @@
 # 2.1.13   LAB   Formatting the output
 # See the README file, to unederstand the LAB Task
 
-a = "        *\n      *   *\n     *     *\n    *       *\n   *         *\n  *           *\n *             *\n*****       *****\n    *       *\n    *       *\n    *       *\n    *********"
-#print("        *\n      *   *\n     *     *\n    *       *\n   *         *\n  *           *\n *             *\n*****       *****")
-print(a,a)
-#print("  *   *")
-#print(" *     *")
-#print("***   ***")
-#print("  *   *")
-#print("  *   *")
-#print("  *****")
+
+print("        *        "*2)
+print("       * *       "*2)
+print("      *   *      "*2)
+print("     *     *     "*2)
+print("    *       *    "*2)
+print("   *         *   "*2)
+print("  *           *  "*2)
+print(" *             * "*2)
+print("******     ******"*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *     *     "*2)
+print("     *******     "*2)
