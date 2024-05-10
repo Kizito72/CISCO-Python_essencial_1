78a10a78a4437f7e1e5085c6b2d25c603471aaad
Kizito Ngonye <kizitorawbiz@gmail.com>
2024-05-11 00:54:58 +0100

    simple converter

---
+++ LAB_2.4.9__Variables a simple converter/a_simple_converter.py
@@ -0,0 +1,11 @@
+# 2.4.9   LAB   Variables ‒ a simple converter
+# See the README file, to unederstand the LAB Task
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.6
+kilometers_to_miles = kilometers / 1.6
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
