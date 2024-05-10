05b2b26c128b8592b8eedd1d8f7af59f92d756bf
Kizito Ngonye <kizitorawbiz@gmail.com>
2024-05-10 01:07:06 +0100

    dublicating and formating the arrow

---
+++ LAB_2.1.13___Formatting the output/arrow_dublicare.py
@@ -0,0 +1,11 @@
+# 2.1.13   LAB   Formatting the output
+# See the README file, to unederstand the LAB Task
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
