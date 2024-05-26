51de556f32778a576886b81bbef6f9e5e6568ded
Kizito Ngonye <kizitorawbiz@gmail.com>
2024-05-26 19:05:11 +0100

    simple input and output arithmetic operation in python

---
+++ LAB_2.6.9___Simple input and output/simple_input_and_output.py
@@ -0,0 +1,27 @@
+# 2.6.9   LAB   Simple input and output
+# See the README file, to unederstand the LAB Task
+
+# input a float value for variable a here
+num1 = float(input("\n\nEnter first number: \n"))
+
+# input a float value for variable b here
+num2 = float(input("\nEnter second number: \n"))
+
+# output the result of addition here
+add = num1 + num2
+
+# output the result of subtraction here
+subtrat = num1 - num2
+
+# output the result of multiplication here
+multiply = num1 * num2
+
+# output the result of division here
+divid = num1 / num2
+
+print("\n\nFirst number + second number = ",add, 
+      "\n\nFirst number - second number = ",subtrat, 
+      "\n\nFirst number * second number = ",multiply, 
+      "\n\nFirst number / second number = ",divid)
+
+print("\nThat's all, folks!\n")
