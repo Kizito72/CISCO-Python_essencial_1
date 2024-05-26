060166ebc56c363ad144441696c84fdb2f74e04e
Kizito Ngonye <kizitorawbiz@gmail.com>
2024-05-26 18:42:03 +0100

    soved maths evaluation equation in Python Operators and expressions

---
+++ LAB_2.4.10___Operators and expressions/maths_evaluation_equation.py
@@ -0,0 +1,12 @@
+# 2.4.10   LAB   Python Operators and expressions
+# See the README file, to unederstand the LAB Task
+
+# declaring the input values of x
+x = int(input("Enter the first value of x: \n"))
+x = float(x)
+
+# evaluating the equation 3x^3 - 2x^2 + 3x - 1 and 
+# storing the value to y
+y = 3*(x**3) - 2*(x**2) + (3*x) - 1
+
+print("y = ", y)
